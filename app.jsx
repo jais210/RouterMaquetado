a9605193ac372b04e89c1549e044d1a46c701e18
jais210 <jakeline_210@hotmail.com>
2017-10-06 18:24:27 -0500

    courses

--- app.jsx
+++ app.jsx
@@ -1,3 +1,4 @@
+// arreglo de profesores
 const teacherList = [
   {
     img: "img/angieMcAngular.png",
@@ -37,43 +38,43 @@ const teacherList = [
   }
 
 ];
-
-const coursesList = [
+// arreglo de cursos
+const courseList = [
   {
-    img: "img/angie.png",
-    name: "Angie McAngular",
+    img: "img/howToMakeWebsite.png",
+    course: "How To Make a Website",
     information:
-      "Angie is a web developer and teacher who is passionate about building scalable, data driven web apps, especially ones that address old problems with new tech!"
+      "If you’ve never built a website before and you have no coding or design experience, this is the place to start. In this project, we learn how to build a modern portfolio website for desktops, tablets, and mobile devices. We start with basic HTML and CSS syntax. Next, we learn how to build custom web pages with an image gallery and contact page. Finally, we walk through how to share a website live on the web."
   },
   {
-    img: "img/ecma.png",
-    name: "NodeStradamus",
+    img: "img/htmlForms.png",
+    course: "HTML Forms",
     information:
-      "'NodeStra' is a software engineer and philosopher trying to leave the world better than he found it. He codes for non-profits, eCommerce, and large-scale web apps."
+      "The web is a two-way communication medium. There’s lots of HTML elements for displaying data and producing output, and conversely, there’s also lots of HTML elements for accepting input. Accepting input from the user means creating web forms. In this course, we’ll learn about all the most important form elements that web professionals use on a daily basis."
   },
   {
-    img: "img/geo.png",
-    name: "Geo 'Lo' Cation",
+    img: "img/htmlVideoAndAudio.png",
+    course: "HTML Video And Audio",
     information:
-      "Geo is a JavaScript developer working on large-scale applications. He's also a teacher who strives to support students in removing all barriers to learning code."
+      "Text and images have always been the foundation of web content, but more than ever, video and audio are also a part of that content mix. Fortunately, we can now create standards-based video and audio players that don't require the use of plugins. Adding video and audio to a webpage is almost as easy as adding an image or formatting some text."
   },
   {
-    img: "img/jay.png",
-    name: "Geo 'Lo' Cation",
+    img: "img/svgBasics.png",
+    course: "SVG Basics",
     information:
-      "Angie is a web developer and teacher who is passionate about building scalable, data driven web apps, especially ones that address old problems with new tech!"
+      "Scalable Vector Graphics (SVG) is an XML markup language for creating two-dimensional images using vectors. This is different from traditional raster-based image formats that use pixels, like JPEG and PNG. When used on web pages, SVG images provide an infinite level of detail, so they look sharp regardless of screen size or pixel density."
   },
   {
-    img: "img/json.png",
-    name: "Jay Query",
+    img: "img/responsiveImages.png",
+    course: "Responsive Images",
     information:
-      "Angie is a web developer and teacher who is passionate about building scalable, data driven web apps, especially ones that address old problems with new tech!"
+      "Using the new source-set and sizes attributes, and the new picture element, it's possible to create images that behave better in a responsive design. These new pieces of markup allow us to deliver the right image to the right device, based on resolution, pixel density, and other factors we define. This will help web pages load faster and look better, and the most capable devices will get the best looking images possible"
   },
   {
-    img: "img/nodestradamus.png",
-    name: "NodeStradamus",
+    img: "img/introductionHtmlAndCss.png",
+    course: "Introduction to HTML And CSS",
     information:
-      "'NodeStra' is a software engineer and philosopher trying to leave the world better than he found it. He codes for non-profits, eCommerce, and large-scale web apps."
+      "Get started creating web pages with HTML and CSS, the basic building blocks of web development. HTML, or Hypertext Markup Language, is a standard set of tags you will use to tell the web browser how the content of your web pages and applications are structured. Use CSS, or Cascading Style Sheets, to select HTML tags and tell the browser what your content should look like."
   }
 
 ];
@@ -141,7 +142,7 @@ class Teachers extends React.Component {
           <div>
               {teacherList.map((item, index)=>{
                 return (<div key={index}>
-                    <span><img src={item.img}/></span> 
+                    <img src={item.img}/>
                     <span>{item.name}</span>
                     <div>{item.information}</div>
                   </div>
@@ -160,7 +161,7 @@ class Repos extends React.Component {
     let CurrentList = null;
     switch (route) {
       case "css":
-        CurrentList = ["How to Make a CSS", "HTML CSS"].map((item, index) => {
+        CurrentList = courseList.map((item, index) => {
           return <li key={index}> {item} </li>;
         });
         break;
